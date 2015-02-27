389459c2dd87321ab382a85f023f1a15801d2512
卜错 <baiming.org@gmail.com>
2015-02-27 21:31:54 +0800

    array sort

--- arraySort/ViewController.m
+++ arraySort/ViewController.m
@@ -19,15 +19,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    Student *stu1 = [Student initStudentWithFirstname:@"a" withLastname:@"b"];
-    Student *stu2 = [Student initStudentWithFirstname:@"c" withLastname:@"d"];
-    Student *stu3 = [Student initStudentWithFirstname:@"e" withLastname:@"f"];
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn.layer.borderWidth = 1;
+    btn.layer.borderColor = [UIColor grayColor].CGColor;
+    btn.translatesAutoresizingMaskIntoConstraints = NO;
+    [btn setTitle:@"双色球" forState:UIControlStateNormal];
+//    [btn addTarget:self action:@selector(todayExtension:) forControlEvents:UIControlEventTouchUpInside];
     
-    NSArray *array = [NSArray arrayWithObjects:stu1,stu2,stu3, nil];
     
-    NSArray *sortArray = [array sortedArrayUsingSelector:@selector(compareStudent:)];
+    UIButton *dltBtn = [UIButton buttonWithType:UIButtonTypeSystem];
+    dltBtn.layer.borderWidth = 1;
+    dltBtn.layer.borderColor = [UIColor grayColor].CGColor;
+    dltBtn.translatesAutoresizingMaskIntoConstraints = NO;
+    [dltBtn setTitle:@"大乐透" forState:UIControlStateNormal];
+//    [dltBtn addTarget:self action:@selector(todayExtension:) forControlEvents:UIControlEventTouchUpInside];
     
-    NSLog(@"%@",sortArray);
+    [self.view addSubview:btn];
+    [self.view addSubview:dltBtn];
+    
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"H:|[btn(dltBtn)]-10-[dltBtn]|"
+                               options:NSLayoutFormatAlignAllTop|NSLayoutFormatAlignAllBottom
+                               metrics:nil
+                               views:@{@"btn":btn,@"dltBtn":dltBtn}]];
+    
+    [self.view addConstraints:[NSLayoutConstraint
+                               constraintsWithVisualFormat:@"V:|-5-[btn(height)]|"
+                               options:0
+                               metrics:@{@"height":@30}
+                               views:@{@"btn":btn}]];
+    
+//    Student *stu1 = [Student initStudentWithFirstname:@"a" withLastname:@"b"];
+//    Student *stu2 = [Student initStudentWithFirstname:@"c" withLastname:@"d"];
+//    Student *stu3 = [Student initStudentWithFirstname:@"e" withLastname:@"f"];
+//    
+//    NSArray *array = [NSArray arrayWithObjects:stu1,stu2,stu3, nil];
+//    
+//    NSArray *sortArray = [array sortedArrayUsingSelector:@selector(compareStudent:)];
+//    
+//    NSLog(@"%@",sortArray);
 }
 
 - (void)didReceiveMemoryWarning {
